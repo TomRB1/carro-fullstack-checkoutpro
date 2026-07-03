7db760d2b2f4be63ec52eee0a1a70e48fc6c712a
Tomás Rebelen <totocabj2007@gmail.com>
2026-07-03 16:06:52 -0300

    MP

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
+import os
+import mercadopago
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import List, Union
+
+load_dotenv()
+
+MERCADOPAGO_TOKEN = os.getenv("TOKEN_DESARROLLO")
+sdk = mercadopago.SDK(MERCADOPAGO_TOKEN)
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 app.add_middleware(
@@ -13,31 +23,42 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
-class Producto(BaseModel):
-    id: int
+class ItemCarrito(BaseModel):
+    id: Union[int, str]
     title: str
     unit_price: float
     quantity: int
 
 class Carrito(BaseModel):
-    items: list[Producto]
-
+    items: List[ItemCarrito]
 
 @app.get("/")
-def inicio():
-    return {"mensaje": "Backend funcionando"}
-
+def root():
+    return {"status": "ok", "message": "API funcionando"}
 
 @app.post("/carrito")
-def recibir_carrito(carrito: Carrito):
+def post_carrito(carrito: Carrito):
 
-    print("Productos recibidos:")
-    print(carrito)
+    preference_data = {
+        "items": [
+            {
+                "title": item.title,
+                "quantity": item.quantity,
+                "unit_price": item.unit_price,
+                "currency_id": "ARS"
+            }
+            for item in carrito.items
+        ]
+    }
+
+    preference_response = sdk.preference().create(preference_data)
+    preference = preference_response["response"]
 
     return {
-        "mensaje": "Carrito recibido correctamente",
-        "productos": carrito.items
+        "id": preference["id"],
+        "init_point": preference["init_point"],
+        "sandbox_init_point": preference["sandbox_init_point"]
     }
